2b9d1b5f527a314041d183488527d3e1f36b1d13
MugiSus <42643211+MugiSus@users.noreply.github.com>
2022-02-03 22:11:02 +0900

    Update src.js

--- src.js
+++ src.js
@@ -1,14 +1,15 @@
 class Floor {
     constructor(imgsrc, z) {
-        this.imgsrc = "./img/" + imgsrc;
+        this.id = imgsrc;
+        this.imgsrc = "./img/" + imgsrc + ".svg";
         this.z = z;
         this.DOMElement = null;
-        this.focused = false;
     }
 
     applyHTMLElement(DOMElement) {
         this.DOMElement = DOMElement;
-        this.setFocused(this.focused);
+        this.unfocus();
+        return DOMElement;
     }
 
     translate(x, y, scrollx, scrolly, scrollz) {
@@ -16,9 +17,16 @@ class Floor {
         console.log(this.DOMElement.style.transform);
     }
 
-    setFocused(focused) {
-        this.focused = focused;
-        this.DOMElement.style.opacity = focused ? 1 : 0.1;
+    focus() {
+        this.focused = true;
+        this.DOMElement.style.opacity = 1;
+        return this.focused;
+    }
+    
+    unfocus() {
+        this.focused = false;
+        this.DOMElement.style.opacity = 0.1;
+        return this.focused;
     }
 }
 
@@ -37,21 +45,43 @@ class Facility {
     }
 }
 
-let scrollx = -200, scrolly = 0, scrollz = 0;
+class CameraPropeties {
+    constructor(_scrollx, _scrolly, _scrollz) {
+        this._scrollx = _scrollx;
+        this._scrolly = _scrolly;
+        this._scrollz = _scrollz;
+    }
 
+    scroll() {
+        facilities.forEach(facility => 
+            facility.translate(this._scrollx, this._scrolly, this._scrollz)
+        );
+    }
 
-const Facilities = [
+    get scrollx() {return this._scrollx}
+    get scrolly() {return this._scrolly}
+    get scrollz() {return this._scrollz}
+
+    set scrollx(value) {this._scrollx = value; this.scroll()}
+    set scrolly(value) {this._scrolly = value; this.scroll()}
+    set scrollz(value) {this._scrollz = value; this.scroll()}
+}
+
+let cam = new CameraPropeties(0, 0, 0);
+const floorsCollection = new Map();
+
+const facilities = [
     new Facility("Education Building", 0, 0, [
-        new Floor("education-building-1f.svg", -1),
-        new Floor("education-building-2f.svg", 0),
-        new Floor("education-building-3f.svg", 1),
-        new Floor("education-building-4f.svg", 2),
+        new Floor("education-building-1f", -1),
+        new Floor("education-building-2f", 0),
+        new Floor("education-building-3f", 1),
+        new Floor("education-building-4f", 2),
     ])
 ];
 
 const containerElement = document.getElementById("container");
 
-Facilities.forEach(facility => {
+facilities.forEach(facility => {
     facility.floors.forEach(floor => {
         const mapElement = document.createElement("img");
         mapElement.classList.add("map");
@@ -59,6 +89,8 @@ Facilities.forEach(facility => {
         containerElement.appendChild(mapElement);
 
         floor.applyHTMLElement(mapElement);
+        floorsCollection.set(floor.id, floor);
     })
-    facility.translate(scrollx, scrolly, scrollz);
 });
+
+cam.scroll();
